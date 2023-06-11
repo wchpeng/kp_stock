308f2517bb302540b4851cfefa0e5ac75f71f0c3
wusir <chaopeng.wu@zseads.com>
2023-06-11 15:33:22 +0800

    股票列表页添加自定义规则

--- src/views/StocklistView.vue
+++ src/views/StocklistView.vue
@@ -3,7 +3,12 @@
     <Col span="18" offset="3">
       <Form :model="formItem" :label-width="120" class="form">
         <FormItem label="股票代码/名字">
-          <Input v-model="formItem.k" placeholder="如：600519" clearable style="width: 200px" />
+          <Input v-model="formItem.k" placeholder="如：600519" clearable style="width: 350px" />
+        </FormItem>
+        <FormItem label="自定义规则">
+          <Select v-model="formItem.rule" style="width:350px">
+            <Option v-for="item in ruleList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
         </FormItem>
         <!-- <FormItem label="名字">
           <Input v-model="formItem.name" placeholder="如：茅台" clearable style="width: 200px" />
@@ -55,6 +60,7 @@
 
 <script setup>
 import { Message } from 'view-ui-plus'
+import { useRouter } from 'vue-router'
 import { ref, reactive, onMounted, watch } from 'vue'
 import { api_stock_list,api_stock_add, api_stock_del} from '@/api/index'
 
@@ -63,6 +69,7 @@ const modal_form = reactive({
   ts_code: '',
   name: ''
 })
+const router = useRouter()
 async function modal_ok(){
   if (!modal_form.ts_code || !modal_form.name){
     Message.warning('填写不规范')
@@ -88,23 +95,31 @@ async function delete_ok(ts_code){
 function delete_cancel(){}
 const formItem = reactive({
   k: '',
+  rule: '1'
 })
+const ruleList = [
+  {label: '------', value: '0'},
+  {label: '流通市值300亿内昨日交易金额大于流通市值5%', value: '1'}
+]
 const loading = ref(false)
 const columns = reactive([
   {title: '股票代码', slot: 'ts_code'},
   {title: '名字', key: 'name'},
   {title: '开盘', key: 'open'},
   {title: '收盘', key: 'close'},
-  {title: '最高', key: 'high'},
-  {title: '最低', key: 'low'},
-  {title: '成交量', key: 'vol'},
-  {title: '成交额', key: 'amount'},
+  // {title: '最高', key: 'high'},
+  // {title: '最低', key: 'low'},
+  // {title: '成交量', key: 'vol'},
+  {title: '交易额', key: 'amount'},
+  {title: '量比', key: 'volume_ratio'},
+  {title: '换手率', key: 'turnover_rate'},
+  {title: '流通市值', key: 'circ_mv'},
   {title: '操作', slot: 'selfc', align: 'center'},
 ])
 // const data = reactive({'values': []})
 const data = reactive([])
 const total = ref(60)
-const pageSize = ref(20)
+const pageSize = ref(10)
 const pageNo = ref(1)
 watch(
   () => formItem.k,
@@ -112,19 +127,35 @@ watch(
     pageNo.value = 1
   }
 )
+watch(
+  () => formItem.rule,
+  (keyword) => {
+    pageNo.value = 1
+    getStockList()
+  }
+)
 async function getStockList(){
+  loading.value = true
   let params = {
     page_no: pageNo.value,
     page_size: pageSize.value,
     k: formItem.k,
+    rule: formItem.rule
   }
   let response = await api_stock_list(params)
-  data.splice(0, data.length)
-  for (let i=0; i<response.data.list.length; i++){
-    data.push(response.data.list[i])
+  if (response.code != 0){
+    loading.value = false
+    Message.warning(response.msg)
+    router.push({path: '/login'})
+  } else {
+    data.splice(0, data.length)
+    for (let i=0; i<response.data.list.length; i++){
+      data.push(response.data.list[i])
+    }
+    // data.values = response.data.list
+    total.value = response.data.total
   }
-  // data.values = response.data.list
-  total.value = response.data.total
+  loading.value = false
 }
 async function changePageNo(){
   await getStockList()
